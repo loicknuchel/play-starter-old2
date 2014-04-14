eec61729725c4a922322af59635a7491781fb63e
ubourdon <bourdon.ugo@gmail.com>
2014-04-14 23:52:08 +0200

    clean: User unsed mongo mapping

--- app/models/User.scala
+++ app/models/User.scala
@@ -9,21 +9,4 @@ object UserJsonFormat {
   import play.api.libs.json.Json
 
   implicit val userFormat = Json.format[User]
-
-  /*implicit object UserBSONReader extends BSONDocumentReader[User] {
-    def read(doc: BSONDocument): User =
-      User(
-        doc.getAs[String]("id"),
-        doc.getAs[String]("name").get,
-        doc.getAs[String]("bio").get)
-  }
-
-  /** serialize a User into a BSON **/
-  implicit object UserBSONWriter extends BSONDocumentWriter[User] {
-    def write(user: User): BSONDocument =
-      BSONDocument(
-        "id" -> user.id/*.getOrElse(BSONObjectID.generate)*/,
-        "name" -> user.name,
-        "bio" -> user.bio)
-  }*/
 }
